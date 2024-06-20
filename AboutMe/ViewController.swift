7a2a661ed72fe992956958580b6af27f93f6d161
uliana <ulianaevstigneeva@yandex.ru>
2024-06-20 16:45:51 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Ульяна Евстигнеева on 20.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
