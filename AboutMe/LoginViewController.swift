785ff231440e97c5bc38d8ac873e6d8d57d0a643
uliana <ulianaevstigneeva@yandex.ru>
2024-06-22 19:17:01 +0300

    done

--- AboutMe/LoginViewController.swift
+++ AboutMe/LoginViewController.swift
@@ -5,46 +5,61 @@
 //  Created by Ульяна Евстигнеева on 20.06.2024.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
-
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwardTF: UITextField!
+    
     private let userName = "Uliana"
     private let passward = "12345"
     
-    @IBOutlet var userNameTV: UITextField!
-    @IBOutlet var passwardTV: UITextField!
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.name = userName
+    }
     
     @IBAction func logInButton() {
-        guard let userNameInput = userNameTV.text, !userNameInput.isEmpty else {
-            showAlert(withMessage: "поле не может быть пустым")
+        guard let userNameInput = userNameTF.text, !userNameInput.isEmpty else {
+            showAlert(
+                withTitle: "field cannot be empty",
+                andMessage: "enter your name"
+            )
             return
         }
-        guard let passwardInput = passwardTV.text, !passwardInput.isEmpty else {
-            showAlert(withMessage: "поле не может быть пустым")
+        guard let passwardInput = passwardTF.text, !passwardInput.isEmpty else {
+            showAlert(
+                withTitle: "field cannot be empty",
+                andMessage: "enter your passward"
+            )
             return
         }
         guard userNameInput == userName, passwardInput == passward else {
-            showAlert(withMessage: "не верный логин или пароль")
+            showAlert(
+                withTitle: "wrong login or password",
+                andMessage: "try again"
+            )
             return
         }
-
     }
     
     @IBAction func showUsername() {
-        showAlert(withMessage: "Uliana")
+        showAlert(withTitle: "your name", andMessage: "Uliana")
     }
     @IBAction func showPassward() {
-        showAlert(withMessage: "12345")
+        showAlert(withTitle: "your passward", andMessage: "12345")
     }
-    
-    private func showAlert(withMessage message: String) {
-        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = nil
+        passwardTF.text = nil
+    }
+
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAktion = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okAktion)
         present(alert, animated: true)
