900859173061d1d55816ec010952a85484d17175
uliana <ulianaevstigneeva@yandex.ru>
2024-06-22 13:58:34 +0300

    2 screens

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+
+    private let userName = "Uliana"
+    private let passward = "12345"
+    
+    @IBOutlet var userNameTV: UITextField!
+    @IBOutlet var passwardTV: UITextField!
+    
+    @IBAction func logInButton() {
+        if userName ==  userNameTV.text && passward == passwardTV.text {
+            // открываем страницу
+        }
     }
-
-
+    
 }
 
